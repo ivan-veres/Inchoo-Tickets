f757d90a0d78870fb6fafb4331a5497b3cfed967
ivan-veres <ivan.veres.dj@gmail.com>
2016-01-22 13:48:13 +0100

    fail

---
+++ app/code/local/Inchoo/Tickets/Block/Adminhtml/Ticket/View/Message.php
@@ -0,0 +1,27 @@
+<?php
+
+class Inchoo_Tickets_Block_Adminhtml_Ticket_View_Message extends Mage_Adminhtml_Block_Template
+{
+    protected function _prepareLayout()
+    {
+        $onclick = "submitAndReloadArea($('ticket_view'), '".$this->getSubmitUrl()."')";
+        $button = $this->getLayout()->createBlock('adminhtml/widget_button')
+            ->setData(array(
+                'label'   => Mage::helper('sales')->__('Submit Message'),
+                'class'   => 'save',
+                'onclick' => $onclick
+            ));
+        $this->setChild('submit_button', $button);
+        return parent::_prepareLayout();
+    }
+
+    public function getTicket()
+    {
+        return Mage::registry('current_ticket');
+    }
+
+    public function getSubmitUrl()
+    {
+        return $this->getUrl('*/*/messagePost', array('ticket_id' => $this->getTicket()->getTicketId()));
+    }
+}
